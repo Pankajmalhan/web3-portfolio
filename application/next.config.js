5b09a1de38a03d3ae3a0fb9a015e85413dc116c1
PankajC-Tft <86830045+PankajC-Tft@users.noreply.github.com>
2023-08-02 21:59:24 +0530

    added template for portfolio

--- application/next.config.js
+++ application/next.config.js
@@ -1,7 +1,9 @@
-/** @type {import('next').NextConfig} */
+/**
+ * @type {import('next').NextConfig}
+ */
+
 const nextConfig = {
   reactStrictMode: true,
-  basePath: "",
 };
 
 module.exports = nextConfig;
